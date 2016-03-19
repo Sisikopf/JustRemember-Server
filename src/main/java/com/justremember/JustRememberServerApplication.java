fbb5b8a68d63700cdc008e7d52d2480c3bd0fd26
sisikopf <dimko12@gmail.com>
2016-03-19 13:35:52 +0300

    working

--- src/main/java/com/justremember/JustRememberServerApplication.java
+++ src/main/java/com/justremember/JustRememberServerApplication.java
@@ -1,9 +1,18 @@
 package com.justremember;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
 public class JustRememberServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/justremember/entities/User.java
@@ -0,0 +1,34 @@
+package com.justremember.entities;
+
+/**
+ * Created by dimko_000 on 19.03.2016.
+ */
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
